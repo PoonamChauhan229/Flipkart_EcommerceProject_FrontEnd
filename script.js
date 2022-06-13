3945bf74a05080d0d34ced55be780668fb460322
Poonam <pooonam.chauhan229@gmail.com>
2022-06-14 02:57:31 +0530

    dark mode and coupon

---
+++ script.js
@@ -0,0 +1,21 @@
+var coupon=document.getElementById('coupon')
+
+function loadCoupon(){
+coupon.style.visibility="visible"
+
+}
+
+function closeCoupon(){
+coupon.style.visibility="hidden"
+}
+
+function changeMode(){
+    document.body.classList.toggle('mydark')
+    
+    document.getElementById('darkmode').classList.toggle('mydark')
+
+
+    
+}
+
+window.onload=loadCoupon();
