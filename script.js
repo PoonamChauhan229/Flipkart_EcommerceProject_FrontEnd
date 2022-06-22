a7fa6dfb36cdf3ccef09d37c109a97ab508b00c5
Poonam <pooonam.chauhan229@gmail.com>
2022-06-22 20:20:45 +0530

    coupon

--- script.js
+++ script.js
@@ -2,29 +2,28 @@ var coupon=document.getElementById('coupon')
 
 function loadCoupon(){
 coupon.style.visibility="visible"
-document.getElementById('mainDarkMode').classList.add('overlay')
+document.getElementById('mainDarkMode0').classList.add('overlay')
 
 }
 
 function closeCoupon(){
 coupon.style.visibility="hidden"
-document.getElementById('mainDarkMode').classList.remove('overlay')
+document.getElementById('mainDarkMode0').classList.remove('overlay')
 
 
 }
 
 function changeMode(){
-    document.body.classList.toggle('mydark')
-    
+    document.body.classList.toggle('mydark')    
     document.getElementById('darkmode').classList.toggle('mydark')
-    document.getElementById('mainDarkMode').classList.toggle('mydark')
+    document.getElementById('mainDarkMode0').classList.toggle('mydark')
     document.getElementById('mainDarkMode1').classList.toggle('mydark')
     document.getElementById('mainDarkMode2').classList.toggle('mydark')
     document.getElementById('mainDarkMode3').classList.toggle('mydark')
-
-
-    document.getElementsByClassName('marginContainer').classList.toggle('mydark')
-
+    document.getElementById('mainDarkMode4').classList.toggle('mydark')
+    document.getElementById('mainDarkMode5').classList.toggle('mydark')
+    document.getElementById('mainDarkMode6').classList.toggle('mydark')
+    document.getElementById('mainDarkMode7').classList.toggle('mydark')
 }
 
 window.onload=loadCoupon();
