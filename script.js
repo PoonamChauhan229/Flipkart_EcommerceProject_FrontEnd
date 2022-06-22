c56401da04784fd358b16732f1fe04831df938b6
Poonam <pooonam.chauhan229@gmail.com>
2022-06-22 19:32:11 +0530

    coupoun

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-// var coupon=document.getElementById('coupon')
+var coupon=document.getElementById('coupon')
 
-// function loadCoupon(){
-// coupon.style.visibility="visible"
+function loadCoupon(){
+coupon.style.visibility="visible"
 
-// }
+}
 
-// function closeCoupon(){
-// coupon.style.visibility="hidden"
-// }
+function closeCoupon(){
+coupon.style.visibility="hidden"
+}
 
 function changeMode(){
     document.body.classList.toggle('mydark')
     
     document.getElementById('darkmode').classList.toggle('mydark')
+    document.getElementById('mainDarkMode').classList.toggle('mydark')
 
 
     document.getElementsByClassName('marginContainer').classList.toggle('mydark')
+
 }
 
 window.onload=loadCoupon();
